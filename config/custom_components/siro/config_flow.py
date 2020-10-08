ba7e0e71f4d73eeecd6bf1a5e630be24ca7ae5b7
Felix Arnold <moin@felix-arnold.dev>
2020-10-08 22:14:13 +0200

    changing to public siro package

--- config/custom_components/siro/config_flow.py
+++ config/custom_components/siro/config_flow.py
@@ -11,18 +11,6 @@ from .const import DOMAIN
 # from .siro.siro import Bridge, Helper
 
 _LOGGER = logging.getLogger(__name__)
-
-# This is the schema that used to display the UI to the user. This simple
-# schema has a single required host field, but it could include a number of fields
-# such as username, password etc. See other components in the HA core code for
-# further examples.
-# Note the input displayed to the user will be translated. See the
-# translations/<lang>.json file and strings.json. See here for further information:
-# https://developers.home-assistant.io/docs/config_entries_config_flow_handler/#translations
-# At the time of writing I found the translations created by the scaffold didn't
-# quite work as documented and always gave me the "Localise key references" string
-# (in square brackets), rather than the actual translated value. I did not attempt to
-# figure this out or look further into it.
 DATA_SCHEMA = {
     vol.Required("title", description="name of the integration", default="SIRO Bridge"): str,
     vol.Required("key", description="Connector+ account key"): str,
@@ -72,36 +60,24 @@ class SiroConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
     """
     Handle a config flow for SIRO.
     """
-
     VERSION = 1
-    # Pick one of the available connection classes in home assistant/config_entries.py
-    # This tells HA if it should be asking for updates, or it'll be notified of updates
-    # automatically. This example uses PUSH, as the dummy hub will notify HA of
-    # changes.
     CONNECTION_CLASS = config_entries.CONN_CLASS_LOCAL_PUSH
 
     async def async_step_user(self, user_input=None):
-        """Handle the initial step."""
-        # This goes through the steps to take the user through the setup process.
-        # Using this it is possible to update the UI and prompt for additional
-        # information. This example provides a single form (built from `DATA_SCHEMA`),
-        # and when that has some validated input, it calls `async_create_entry` to
-        # actually create the HA config entry. Note the "title" value is returned by
-        # `validate_input` above.
+        """
+        Handle the initial step.
+        """
+
         errors = {}
         if user_input is not None:
             # noinspection PyBroadException
             try:
                 print(f"user_input in step_user input: {user_input}")
                 info = await validate_input(self.hass, user_input)
-                return self.async_create_entry(title=info["title"], data=user_input)
+                return self.async_create_entry(title=info["title"], data=info)
             except CannotConnect:
                 errors["base"] = "cannot_connect"
             except InvalidHost:
-                # The error string is set here, and should be translated.
-                # This example does not currently cover translations, see the
-                # comments on `DATA_SCHEMA` for further details.
-                # Set the error on the `host` field, not the entire form.
                 errors["title"] = "cannot_connect"
             except InvalidKey:
                 errors["title"] = "wrong_key"
@@ -109,8 +85,6 @@ class SiroConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
                 _LOGGER.exception("Unexpected exception")
                 errors["base"] = "unknown"
 
-        # If there is no user input or there were errors, show the form again, including any errors that were
-        # found with the input.
         return self.async_show_form(
             step_id="user", data_schema=vol.Schema(DATA_SCHEMA), errors=errors
         )
